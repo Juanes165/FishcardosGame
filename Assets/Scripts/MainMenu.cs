b0756e07e98e7ecde583edbffbe2f79508f60972
Juanda2207 <71685486+Juanda2207@users.noreply.github.com>
2020-12-10 03:13:41 -0500

    Ajustes varios
    Algunos ajustes a la barra de vida, implementado el menú principal con un efecto de parpadeo

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            StartGame();
+        }
+    }
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Assets/Scripts/ParpadeoTexto.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ParpadeoTexto : MonoBehaviour
+{
+    private Text textoParpadeante;
+    private string textoQueParpadea = "Press any key to play";
+    private string textoEnBlanco = "";
+    private string textoEstatico = "No parpadea";
+    private bool estaParpadeando = true;
+
+    private void Start()
+    {
+        textoParpadeante = GetComponent<Text>();
+
+        StartCoroutine(TextoParpadeo());
+        
+    }
+
+    private IEnumerator TextoParpadeo()
+    {
+        while (estaParpadeando)
+        {
+            textoParpadeante.text = textoEnBlanco;
+            
+            yield return new WaitForSeconds(0.5f);
+
+            textoParpadeante.text = textoQueParpadea;
+            
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
