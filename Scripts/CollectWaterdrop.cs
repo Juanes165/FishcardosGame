19339f0b1a7000768e51c5abc44b4433561a781e
Juanda2207 <71685486+Juanda2207@users.noreply.github.com>
2020-12-10 22:03:19 -0500

    Add files via upload

---
+++ Scripts/CollectWaterdrop.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectWaterdrop : MonoBehaviour
+{
+    public Animator animator;
+    Animation animation;
+    AnimatorClipInfo[] animatorClipInfo;
+
+    public Image image;
+
+
+    private void Start()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if(collision.CompareTag("Player"))
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+            Destroy(gameObject, 0f);
+            FindObjectOfType<OxigenCounter>().dropCollected();
+        }
+    }
+}
---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 250f;
+    public float jumpSpeed = 200f;
+    public float fallMultiplier = 0.5f;
+    public float lowJumpMultiplier = 1f;
+    Rigidbody2D rigidBody;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //MOVERSE, IZQUIERDA Y DERECHA
+        if (Input.GetKey("a"))
+        {
+            rigidBody.AddForce(new Vector2(-speed * Time.deltaTime, 0));
+            animator.SetBool("moving", true);
+            spriteRenderer.flipX = true;
+        }
+        if (Input.GetKey("d"))
+        {
+            rigidBody.AddForce(new Vector2(speed * Time.deltaTime, 0));
+            animator.SetBool("moving", true);
+            spriteRenderer.flipX = false;
+        }
+        //ESTADO QUIETO
+        if (!Input.GetKey("d") && !Input.GetKey("a"))
+        {
+            animator.SetBool("moving", false);
+        }
+        //VERIFICAR SI ESTÁ EN EL SUELO PARA SALTAR
+        if (Input.GetKeyDown("w") && CheckGround.isGrounded || Input.GetKeyDown("space") && CheckGround.isGrounded)
+        {
+            rigidBody.AddForce(new Vector2(0, jumpSpeed));
+        }
+        //NO MOVERSE SI SE MUEVE HACIA AMBOS LADOS
+        if (Input.GetKey("d") && Input.GetKey("a"))
+        {
+            animator.SetBool("moving", false);
+        }
+        
+        
+        //AGACHARSE
+        
+        if (Input.GetKey("s"))
+        {
+            animator.SetBool("bend", true);
+        }
+
+        if (!Input.GetKey("s"))
+        {
+            animator.SetBool("bend", false);
+        }
+
+        if ((Input.GetKey("s")) && Input.GetKey("a") || (Input.GetKey("s")) && Input.GetKey("d"))
+        {
+            animator.SetBool("move_bend", true);
+        }
+        
+        if (!((Input.GetKey("s")) && Input.GetKey("a") || (Input.GetKey("s")) && Input.GetKey("d")))
+        {
+            animator.SetBool("move_bend", false);
+        }
+
+    }
+}
